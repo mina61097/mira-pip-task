c5cb26235b927881662d2de96c8c77c226a94a2d
mina61097 <mina.mehdizadeh@student.lab.fi>
2026-02-12 09:50:16 +0200

    Refactor temperature converter to use private variable
    update

--- New folder/temperature_converter.py
+++ New folder/temperature_converter.py
@@ -1,22 +1,15 @@
-# temperature_converter.py
-
 class TemperatureConverter:
     def __init__(self):
-        self.temperature = 0.0  # مقدار اولیه دما
+        self.__temperature = 0.0
 
     def setTemperature(self, temp: float) -> None:
-        """مقدار دما را تنظیم می‌کند"""
-        self.temperature = temp
+        self.__temperature = temp
 
     def toCelsius(self) -> float:
-        """دما را به سانتی‌گراد تبدیل می‌کند"""
-        # فرض می‌کنیم مقدار temperature بر حسب سانتی‌گراد است
-        return self.temperature
+        return self.__temperature
 
     def toFahrenheit(self) -> float:
-        """دما را به فارنهایت تبدیل می‌کند"""
-        return (self.temperature * 9/5) + 32
+        return (self.__temperature * 9/5) + 32
 
     def toKelvin(self) -> float:
-        """دما را به کلوین تبدیل می‌کند"""
-        return self.temperature + 273.15
+        return self.__temperature + 273.15
