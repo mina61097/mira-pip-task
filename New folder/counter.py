ed283295ffb3424fbd92fc07f6a28e911d47976b
mina61097 <mina.mehdizadeh@student.lab.fi>
2026-02-12 09:55:03 +0200

    Refactor Counter class to use private count attribute
    update

--- New folder/counter.py
+++ New folder/counter.py
@@ -1,14 +1,15 @@
-# counter.py
-
 class Counter:
     def __init__(self):
-        self.count = 0
+        self.__count = 0  # initialize count to 0
 
-    def addCount(self):
-        self.count += 1
+    def addCount(self) -> None:
+        """Increment the count by 1."""
+        self.__count += 1
 
-    def getCount(self):
-        return self.count
+    def getCount(self) -> int:
+        """Return the current count."""
+        return self.__count
 
-    def zeroCount(self):
-        self.count = 0
+    def zeroCount(self) -> None:
+        """Reset count to 0."""
+        self.__count = 0
